c3250850f5f5cfc49f7e5bbe980d29be1c387483
Naphat <naphat.d@gmail.com>
2021-07-31 23:36:06 +0700

    basic python

---
+++ main.py
@@ -0,0 +1,54 @@
+# Comparison operators
+
+"""
+Operator	        Name	                    Example
+<	            Less than	                    a < b
+<=	            Less than or equal	            a <= b
+>	            Greater than	            a > b
+>=	            Greater than or equal	    a >= b
+==	            Equal	                    a == b
+!=	            Not equal	                    a != b
+is	            Object identity	            a is b
+is not	            Negated object identity	    a is not b
+"""
+# x, y = 5, 5
+# print("1: ", x > y)
+# print("2: ", x < y)
+# print("3: ", x == y)
+# print("4: ", x is y)
+# print("5: ", x is not y)
+# print("6: ", x != y)
+# print("7: ", x >= y & x < y)
+
+#--------------------------------#
+
+# Logical operators
+"""
+Operator	Example	Result
+and	a and b	True if a and b are true, else False
+or	a or b	True if a or b are true, else False
+not	not a	True if a is False, else True
+"""
+
+# x = (5 <= 10)
+# y = (10 != 5)
+# z = x and y
+# print(5 > 10 and 10 < 5)
+# print(5 < 10 and 10 > 5)
+# print(5 != 10 and 10 != 5)
+# print('_________')
+# print(5 > 10 or 10 > 5)
+# print(5 > 10 or 10 < 5)
+# print(5 != 10 or 10 == 5)
+
+#--------------------------------#
+
+#  Compound Assignment
+#
+# x = 10
+# print('ก่อนเพิ่มค่า', x)
+# x = x + 5
+# x *= 5
+# print('หลังเพิ่มค่า', x)
+
+#--------------------------------#
